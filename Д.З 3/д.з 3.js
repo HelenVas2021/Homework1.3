3a22978e8770f98efb9f47c94c303d4a40641b07
Елена <90407873+HelenVas2021@users.noreply.github.com>
2022-10-04 14:03:25 +0300

    Add files via upload

---
+++ Д.З 3/д.з 3.js
@@ -0,0 +1,4 @@
+let firstNumber = Number(prompt('Введите первое число'));
+let secondNumber = Number(prompt('Введите второе число'));
+let thirdNumber = Number(prompt('Введите третье число'));
+alert(`Среднее арифметическое трех чисел ${(firstNumber + secondNumber + thirdNumber) / 3}`)
